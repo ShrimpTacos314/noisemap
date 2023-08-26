7a5fa0e2b446599a9e68887e50f2c5d79dce66a4
ShrimpTacos314 <you@example.com>
2023-08-26 11:45:41 -0500

    Created a ton of bugs

--- script.js
+++ script.js
@@ -18,8 +18,24 @@ const loadDivs = (container) => {
 };
 loadDivs(container);
 
-const getNoiseAtCoordinates = ({ x, y }, offset, strength) => {
-	return (perlin.get(offset + x / 20, offset - y / 20) + 1) * strength;
+const getNoiseAtCoordinates = ({ x, y }, offset, config) => {
+	let {res, pos, strength} = JSON.parse(config);
+	return (perlin.get(offset + x / res, offset - y / res) + pos) * strength;
+};
+
+const test = ({ x, y }, offset) => {
+	let toReturn = (perlin.get(offset + x / 20, offset - y / 20) + 0.1) * 180;
+	return toReturn;
+};
+
+const getNoiseConfig = ({ res, pos, strength }) => {
+	const toReturn = {
+		res: document.querySelector(`#${res}`).value,
+		pos: (document.querySelector(`#${pos}`) != undefined ? document.querySelector(`#${pos}`).value : '1'),
+		strength: document.querySelector(`#${strength}`).value
+	}
+
+	return JSON.stringify(toReturn);
 };
 
 const fillColors = (offset) => {
@@ -27,19 +43,25 @@ const fillColors = (offset) => {
 		for (let j = 0; j < 20; j++) {
 			let space = document.querySelector(`#X${i}Y${j}`);
 
-			space.style = `background-color: hsl(${getNoiseAtCoordinates(
+			space.style = `background-color: hsl(${
+				test({x: i, y: j}, offset)
+			}, 100%, 50%); top: ${getNoiseAtCoordinates(
 				{ x: i, y: j },
 				offset,
-				180
-			)}, 100%, 50%); top: ${getNoiseAtCoordinates(
+				getNoiseConfig({
+					res: "res",
+					pos: "default",
+					strength: "movement"
+				})
+			)}em; right: ${getNoiseAtCoordinates(
 				{ x: i, y: j },
 				offset,
-				35
-			)}px; right: ${getNoiseAtCoordinates(
-				{ x: i, y: j },
-				offset,
-				35
-			)}px`;
+				getNoiseConfig({
+					res: "res",
+					pos: "default",
+					strength: "movement"
+				})
+			)}em`;
 		}
 	}
 
@@ -47,9 +69,12 @@ const fillColors = (offset) => {
 };
 
 let temp = startValue;
-window.setInterval(() => {
-	temp = fillColors(temp);
-}, 30);
+// window.setInterval(() => {
+// 	temp = fillColors(temp);
+// }, 30);
+document.addEventListener("wheel", () => {
+	temp - fillColors(temp);
+})
 
 const fixSize = () => {
 	const sideLength = Math.min(window.innerHeight, window.innerWidth);
